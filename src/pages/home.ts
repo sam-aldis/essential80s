f78ddaa075b5603a5a84d577c12910dce18321d9
Sam Aldis <sam.aldis@ukjp.app>
2019-11-30 12:23:54 +0000

    Sending PC abroad

---
+++ src/pages/home.ts
@@ -0,0 +1,12 @@
+﻿import {html} from 'lit-html';
+export const markupHome = html`<div class="content--center">
+<h3>Available for</h3>
+<ul>
+<li><i class="far fa-check-circle"></i> Weddings</li>
+<li><i class="far fa-check-circle"></i> Corporate Events</li>
+<li><i class="far fa-check-circle"></i> Functions</li>
+<li><i class="far fa-check-circle"></i> Festivals</li>
+<li><i class="far fa-check-circle"></i> Clubs & More!</li>
+</ul>
+<p class="content--tagline">Book The Act that will FILL Your Dance Floor From Start to Finish, Guaranteed!</p>
+</div>`;
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,4 +1,5 @@
-﻿type Page = "home" | "theshow" | "repertoire" | "media" | "gallery" | "bookings";
+﻿import { markupHome } from "~pages/home";
+type Page = "home" | "theshow" | "repertoire" | "media" | "gallery" | "bookings";
 interface IRouter {
 	currentPage : Page;
 	nextPage : Page;
@@ -6,31 +7,41 @@ interface IRouter {
 }
 interface IRouterLink {
 	name: string;
-	linkElement: HTMLElement;
+	template: HTMLElement | string;
 }
+const links : IRouterLink[] = [
+	{
+		name: "home",
+		template: markupHome.getHTML()
+	},
+	{
+		name : "theshow",
+		template: ""
+	}
+];
 class Router implements IRouter {
 	public currentPage : Page = (document.location.hash !== undefined) ? ((document.location.hash.replace("#", "") as Page) ? document.location.hash.replace("#", "") as Page : "home") : "home";
 	public nextPage : Page = "home";
+	constructor(private container : HTMLElement) {
+
+	}
 	get page() {
 		this.nextPage = (document.location.hash !== undefined) ? ((document.location.hash.replace("#", "") as Page) ? document.location.hash.replace("#", "") as Page : "home") : "home";
 		this.Navigate(this.nextPage);
 		return this.nextPage;
 	}
 	Navigate(page : Page) : void {
-		let newElement = (document.querySelector(`#${page}`) as HTMLElement);
-		let oldElement = (document.querySelector(`#${this.currentPage}`) as HTMLElement);
+		
 		let newLink = (document.querySelector(`#NAV${page}`) as HTMLElement);
 		let oldLink = (document.querySelector(`#NAV${this.currentPage}`) as HTMLElement);
 		console.log(oldLink);
 		this.currentPage = page;
-		oldElement.classList.add("article--hidden");
-		newElement.classList.remove('article--hidden');
 		oldLink.classList.remove("current--page");
 		newLink.classList.add("current--page");
 	}
 }
 
-const router = new Router();
+const router = new Router((document.querySelector("#content") as HTMLElement));
 const routerLink = (page : Page)=> {
 	router.Navigate(page);
 }
@@ -38,16 +49,5 @@ window.onhashchange = ()=> {
 	console.log(router.page);
 }
 document.onload = () => {
-	const links : IRouterLink[] = [
-		{
-			name: "home",
-			linkElement: (document.querySelector("#NAVhome") as HTMLElement)
-		},
-		{
-			name : "theshow",
-			linkElement: (document.querySelector("#NAVtheshow") as HTMLElement)
-		}
-	]
 	
-	// links.forEach((el) => el.linkElement.addEventListener("click", ()=>routerLink(el.name as Page)));
 }
